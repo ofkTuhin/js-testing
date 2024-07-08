758cf145ad27bbec3d277e01b17521d065fdcef1
ofkTuhin <tuhin.omar99@gmail.com>
2024-07-08 08:44:27 +0600

    add integration test

--- utits.test.js
+++ utits.test.js
@@ -1,6 +1,12 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
 test("should return name and age", () => {
   const test = generateText("tuhin", 29);
   expect(test).toBe("tuhin (29 years old)");
+});
+
+// intgration tests
+test("should return name and age", () => {
+  const test = checkAndGenerate("Tuhin", 29);
+  expect(test).toBe("Tuhin (29 years old)");
 });
